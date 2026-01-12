c0d886192832f7a195638f1fb950d5c7b0d63092
Emily Palaska <aimilia.p2@gmail.com>
2026-01-12 15:31:58 +0100

    env factory function with regards to env id

--- env_abstraction.py
+++ env_abstraction.py
@@ -1,10 +1,21 @@
 import mo_gymnasium as mo_gym
 from mo_gymnasium.wrappers.vector import MOSyncVectorEnv
 
-def env_factory(env_id:str = "deep-sea-treasure-v0"):
-    return mo_gym.make(env_id)
+env_ids = {
+    "deep sea treasure": "deep-sea-treasure-v0",
+    "minecart": "minecart-v0",
+}
+
+def make_env_factory(env_id:str):
+    def _factory(): return mo_gym.make(env_id)
+    return _factory
+
 
 def initialize_envs(env_id:str = "deep-sea-treasure-v0"):
-    env = MOSyncVectorEnv(iter([env_factory]))
+    env = MOSyncVectorEnv(iter([make_env_factory(env_id)]))
     eval_env = mo_gym.make(env_id)
     return env, eval_env
+
+
+def dst_ground_truth(env):
+    raise NotImplementedError
