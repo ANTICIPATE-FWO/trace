0b04d55523490e1b945e2728f72ef5f943adf12f
Emily Palaska <aimilia.p2@gmail.com>
2026-01-13 16:45:22 +0100

    gather files related to environments into setting module

---
+++ policy_explainability/setting/env_data.py
@@ -0,0 +1,10 @@
+env_data = {
+    "deep-sea-treasure-v0": {
+        "ref_point": [-100.0, -100.0],
+        "file_prefix": "dst"
+    },
+    "minecart-v0": {
+        "ref_point": [-100.0, -100.0, -100.0],
+        "file_prefix": "mc"
+    }
+}
--- policy_explainability/setting/setting.py
+++ policy_explainability/setting/setting.py
@@ -1,11 +1,6 @@
 import mo_gymnasium as mo_gym
 from mo_gymnasium.wrappers.vector import MOSyncVectorEnv
 
-env_ids = {
-    "deep sea treasure": "deep-sea-treasure-v0",
-    "minecart": "minecart-v0",
-}
-
 def make_env_factory(env_id:str):
     def _factory(): return mo_gym.make(env_id)
     return _factory
@@ -14,14 +9,9 @@ def make_env_factory(env_id:str):
 def initialize_setting(env_id:str = "deep-sea-treasure-v0"):
     env = MOSyncVectorEnv(iter([make_env_factory(env_id)]))
     eval_env = mo_gym.make(env_id)
+    from policy_explainability.setting.env_data import env_data
+    return env, eval_env, env_data[env_id]["ref_point"], env_data[env_id]["file_prefix"]
 
-    #file path
-    #ref point
-    return env, eval_env
-
-
-def assign_ref_point(env_id:str):
-    pass
 
 def dst_ground_truth(env):
     raise NotImplementedError
