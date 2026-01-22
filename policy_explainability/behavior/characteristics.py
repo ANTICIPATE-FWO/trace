d4c20bbcff48aeb94a40053ab37612ad7aa4d43a
Emily Palaska <aimilia.p2@gmail.com>
2026-01-22 15:23:55 +0100

    todo

---
+++ policy_explainability/behavior/characteristics.py
@@ -0,0 +1,11 @@
+def cluster_report(actions, rewards):
+    """
+    this function should output the characteristics of a behavioral cluster for a qualitative evluation
+
+    examples:
+    - actions distributions
+    - action sequences
+    - episode length
+
+    """
+    raise NotImplementedError
