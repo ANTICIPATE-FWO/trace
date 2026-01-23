1092572dff9f8a6fd6df73fc1b12e77b192dbc0c
Emily Palaska <aimilia.p2@gmail.com>
2026-01-23 16:12:46 +0100

    temporary holder for data manager

---
+++ trace/simulation/trajectory.py
@@ -0,0 +1,7 @@
+class Trajectory:
+    """
+    maybe this will be a unified data structure to aid data processing across code
+    to be decided
+    """
+    def __init__(self):
+        raise NotImplementedError()
