60e5b64ce1244ff8fa9cd617be24b4b57479cc18
Emily Palaska <aimilia.p2@gmail.com>
2026-01-26 10:35:08 +0100

    add dirichlet mixture and split clustering modules into two files

--- trace/clustering/methods.py
+++ trace/clustering/methods.py
@@ -1,19 +1,8 @@
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
 from sklearn_extra.cluster import KMedoids
-
-def random_cluster(seed:int=42):
-    np.random.seed(seed)
-    center = (np.random.randn(2,)*10).astype(int)
-    return np.random.randn(50, 2) + center
-
-
-def synthetic_dataset(clusters:int=3, seed:int=42):
-    np.random.seed(seed)
-    data = np.vstack([random_cluster(seed + i) for i in range(clusters)])
-    np.random.shuffle(data)
-    return data
+from trace.clustering.auxiliary import reindex_clusters
 
 
 def k_means(data, k: int = 3):
@@ -21,10 +10,12 @@ def k_means(data, k: int = 3):
     kmeans.fit(data)
     return kmeans.labels_, kmeans.cluster_centers_
 
+
 def k_medoids(data, k: int = 3, metric: str = "euclidean"):
     kmedoids = KMedoids(n_clusters=k, metric=metric, init="k-medoids++")
     kmedoids.fit(data)
     return kmedoids.labels_, kmedoids.cluster_centers_
+
 
 def gaussian_mixture(data, k_max: int = 10, covariance_type: str = "full"):
     best_gmm, best_bic = None, np.inf
@@ -47,15 +38,27 @@ def gaussian_mixture(data, k_max: int = 10, covariance_type: str = "full"):
     return best_gmm.predict(data), best_gmm.means_
 
 
-def cluster_connections(labels):
-    k, l = len(np.unique(labels[0])), len(np.unique(labels[1]))
-    source, target, value = [], [], []
+def dirichlet_process_mixture(
+    data,
+    k_max: int = 20,
+    covariance_type: str = "full",
+    weight_concentration_prior: float = 1.0
+):
+    dpgmm = BayesianGaussianMixture(
+        n_components=k_max,
+        covariance_type=covariance_type,
+        weight_concentration_prior_type="dirichlet_process",
+        weight_concentration_prior=weight_concentration_prior,
+        n_init=5,
+        max_iter=1000,
+        tol=1e-4,
+        reg_covar=1e-6,
+        random_state=42,
+        init_params="kmeans"
+    )
 
-    for i in range(k):
-        for j in range(l):
-            count = np.sum((labels[0] == i) & (labels[1] == j))
-            if count > 0:
-                source.append(i)
-                target.append(k + j)
-                value.append(count)
-    return source, target, value
+    dpgmm.fit(data)
+    active = dpgmm.weights_ > 1e-3
+    #weights = dpgmm.weights_[active]
+
+    return reindex_clusters(dpgmm.predict(data)), dpgmm.means_[active]
