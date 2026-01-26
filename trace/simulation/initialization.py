8acb991978c3ee60f174bcf63254ee125d446714
Emily Palaska <aimilia.p2@gmail.com>
2026-01-26 10:38:59 +0100

    move env data into one file for now, will split if it turns out too big

--- trace/simulation/initialization.py
+++ trace/simulation/initialization.py
@@ -1,6 +1,18 @@
 import mo_gymnasium as mo_gym
 from mo_gymnasium.wrappers.vector import MOSyncVectorEnv
 
+env_data = {
+    "deep-sea-treasure-v0": {
+        "ref_point": [-100.0, -100.0],
+        "file_prefix": "dst"
+    },
+    "minecart-v0": {
+        "ref_point": [-100.0, -100.0, -100.0],
+        "file_prefix": "mc"
+    }
+}
+
+
 def make_env_factory(env_id:str):
     def _factory(): return mo_gym.make(env_id)
     return _factory
@@ -9,9 +21,4 @@ def make_env_factory(env_id:str):
 def initialize_setting(env_id:str = "deep-sea-treasure-v0"):
     env = MOSyncVectorEnv(iter([make_env_factory(env_id)]))
     eval_env = mo_gym.make(env_id)
-    from trace.simulation.env_data import env_data
     return env, eval_env, env_data[env_id]["ref_point"], env_data[env_id]["file_prefix"]
-
-
-def dst_ground_truth(env):
-    raise NotImplementedError
