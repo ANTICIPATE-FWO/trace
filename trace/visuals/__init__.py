fe07f9538472fc5e85fecd760aaf70a194e5005d
Emily Palaska <aimilia.p2@gmail.com>
2026-02-02 14:17:30 +0100

    dst arrow policy visualization through most probable action

--- trace/visuals/__init__.py
+++ trace/visuals/__init__.py
@@ -1,3 +1,4 @@
 from trace.visuals.scatters import sankey, cluster_scatter
 from trace.visuals.tsne import tsne_transform
 from trace.visuals.pareto import visualize_pareto
+from trace.visuals.policies import grid_arrows
---
+++ trace/visuals/policies.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from trace.core import env_metadata
+ACTIONS = env_metadata['deep-sea-treasure-v0']['actions']
+
+
+def grid_arrows(policy, width, height, title: str = "Conditioned Policy (Most Probable Action)"):
+    # todo make this versatile to observation spaces that are not boxes
+    x, y = np.meshgrid(np.arange(width), np.arange(height))
+    u = np.full_like(x, np.nan, dtype=float)
+    v = np.full_like(y, np.nan, dtype=float)
+
+    for y in range(height):
+        for x in range(width):
+            obs = np.array([y, x])
+            probs = policy.action_probs(obs)
+            if np.max(probs) - np.min(probs) < 1e-6: continue
+            best_action = np.argmax(probs)
+
+            dx, dy = ACTIONS[best_action]
+            u[y, x] = dx
+            v[y, x] = dy
+
+    plt.figure(figsize=(6, 6))
+    plt.quiver(
+        x, y, u, v,
+        angles="xy",
+        scale_units="xy",
+        scale=1,
+        width=0.015
+    )
+
+    plt.gca().invert_yaxis()
+    plt.xticks(range(width))
+    plt.yticks(range(height))
+    plt.grid(True)
+    plt.title(title)
+    plt.xlabel("y")
+    plt.ylabel("x")
+
+    plt.show()
