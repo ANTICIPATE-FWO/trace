ef64590b1117d57b071f1aacb91affde98ddf257
emily-palaska <aimilia.p2@gmail.com>
2026-02-14 11:48:57 +0100

    simplify grid_arrows function to be more compact and return the figure

--- trace/visuals/policies.py
+++ trace/visuals/policies.py
@@ -2,42 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from trace.core import env_metadata
-ACTIONS = env_metadata['deep-sea-treasure-v0']['actions']
+dst_actions = env_metadata['deep-sea-treasure-v0']['actions']
 
 
-def grid_arrows(policy, title: str = "Conditioned Policy (Most Probable Action)"):
-    #todo the for loop can become cleaner, too many declarations
-    width, height = policy.obs_shape()
-    x, y = policy.obs_space
-    prob_matrix = policy.prob_matrix()
-    u = np.full_like(x, np.nan, dtype=float)
-    v = np.full_like(y, np.nan, dtype=float)
+def grid_arrows(policy, title: str = "Conditioned Policy (Most Probable Action)", color:str = "black"):
+    X, Y = policy.obs_space
+    u, v = np.full_like(X, np.nan), np.full_like(Y, np.nan)
 
-    for y in range(height):
-        for x in range(width):
-            probs = prob_matrix[y, x]
+    for y in Y:
+        for x in X:
+            probs = policy.prob_matrix()[y, x]
             if np.max(probs) - np.min(probs) < 1e-6: continue
-            best_action = np.argmax(probs)
+            u[y, x], v[y, x] = dst_actions[np.argmax(probs)]
 
-            dx, dy = ACTIONS[best_action]
-            u[y, x] = dx
-            v[y, x] = dy
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.quiver(*policy.obs_space, u, v, angles="xy",
+              scale_units="xy",scale=1, width=0.015, color=color,)
 
-    plt.figure(figsize=(6, 6))
-    plt.quiver(
-        *policy.obs_space, u, v,
-        angles="xy",
-        scale_units="xy",
-        scale=1,
-        width=0.015
-    )
+    ax.invert_yaxis()
+    ax.grid(True)
+    ax.set_title(title)
+    ax.set_xlabel("y")
+    ax.set_ylabel("x")
+    return fig
 
-    plt.gca().invert_yaxis()
-    plt.xticks(range(width))
-    plt.yticks(range(height))
-    plt.grid(True)
-    plt.title(title)
-    plt.xlabel("y")
-    plt.ylabel("x")
-
-    plt.show()
