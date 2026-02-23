077c643c8833cc5fae3149d5efb313aca7b27a00
Emily Palaska <aimilia.p2@gmail.com>
2026-02-23 13:39:45 +0100

    add option of precomputed distances

--- trace/visuals/tsne.py
+++ trace/visuals/tsne.py
@@ -2,7 +2,16 @@ import numpy as np
 from sklearn.manifold import TSNE
 
 
-def tsne_transform(data, perplexity: int = 30):
-    perplexity = min(perplexity, len(data) - 1)
-    tsne = TSNE(n_components=2, perplexity=perplexity, init="pca", learning_rate="auto", random_state=42)
-    return tsne.fit_transform(data)
+def tsne_transform(data, similarity: bool = False, perplexity: int = 15):
+    if similarity: assert data.shape[0] == data.shape[1], f"Invalid similarity matrix shape: {data.shape}"
+
+    tsne = TSNE(
+        n_components = 2,
+        perplexity = min(perplexity, (len(data) - 1) // 3),
+        metric = 'precomputed' if similarity else 'euclidean',
+        init = 'random' if similarity else 'pca',
+        learning_rate = "auto",
+        random_state = 42
+    )
+
+    return tsne.fit_transform(1.0 - data) if similarity else tsne.fit_transform(data)
