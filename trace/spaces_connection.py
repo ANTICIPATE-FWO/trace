26e742084062af2e063075b39481660f3ab0fce1
Emily Palaska <aimilia.p2@gmail.com>
2026-04-08 15:26:48 +0200

    make structure clearer, split to load cluster plot and save sections

--- trace/spaces_connection.py
+++ trace/spaces_connection.py
@@ -4,65 +4,66 @@ np.set_printoptions(threshold=10000, suppress=True)
 import os
 os.chdir("..")
 
-from trace.core import  TrajectoryManager, colors, aggregate_policies, tree_features, env_metadata
+from yaml import safe_load
+
+from trace.core import  TrajectoryManager, aggregate_policies, tree_features
 from trace.clustering import k_means, k_medoids, spectral, gaussian_mixture, dirichlet_mixture, cluster_connections
-from trace.visuals import sankey, cluster_scatter, grid_trajectories, grid_arrows, temporal_alignment, decision_tree
+from trace.visuals import sankey, cluster_scatter, grid_trajectories, grid_arrows, temporal_alignment, decision_tree, minecart_trajectories
 from trace.behavior import BayesianPolicy, distance_matrix
 
 # config
-metric = 'agreement'
-k=2
-
-filepath  = "data/9_mc_ipro.json"
-# 38_dst_ipro.json
-#
-env_id = 'minecart-v0'
-plot_directory = f"plots/{env_metadata[env_id]['file_prefix']}_{metric}"
-cluster_functions= [k_medoids, k_medoids]
-
+filepath  = "data/3.2_minecart_ipro.json" # 38_dst_ipro.json
+env_id, method, metric = 'minecart-v0', 'ipro', 'agreement'
+k, cluster_functions= 3, [k_medoids, k_medoids]
 save, show = True, False
-#graph_labels = [(f'TSNE of Conditioned Policies ({metric} metric)', '', ''),
-#                ('Total reward of episode', 'Treasure', 'Time'),]
+
+# global params
+metadata = safe_load(open("trace/configs/environments.yaml", "r"))[env_id]
+colors = safe_load(open("trace/configs/colors.yaml", "r"))
+plot_directory = f"plots/{metadata['file_prefix']}/{method}/{metric}"
 
 
 def main():
-    manager = TrajectoryManager(env_id).load(filepath)
-    rewards = manager.accrued_reward()
-    obs_seq, ac_seq = manager.policy_data(flatten=False, pad=None)
-
-    policies = [BayesianPolicy(env_id, alpha=0.5).fit(obs, acs) for obs, acs in zip(obs_seq, ac_seq)]
+    # Loading
+    manager = TrajectoryManager(metadata).load(filepath)
+    observations, actions, reward_features = manager.policy_data(flatten=False, pad=None, gamma=metadata['gamma'])
+    max_len = max(len(episode) for point in observations for episode in point)
+    policies = [BayesianPolicy(metadata, alpha=0.5).fit(obs, acs) for obs, acs in zip(observations, actions)]
     behavior_features = distance_matrix(policies, metric=metric)
-    print(f'\rInitialized {len(policies)} policies')
 
-    labels, figs = [], []
-    for i, data in enumerate([behavior_features, rewards]):
-        labels.append(cluster_functions[i](data, k=k))
-        figs.append((cluster_scatter(data, labels[-1], color_id=i, precomputed=i==0), f"scatter{i}.png"))
-    figs.append((sankey(*cluster_connections(labels)), "sankey.png"))
+    # Clustering
+    cluster_config = [(k_medoids, behavior_features),
+                      (k_medoids, reward_features)]
+    labels = [cluster(feature, k=k) for cluster, feature in cluster_config]
+    cluster_obs, cluster_acs = aggregate_policies(observations, actions, labels[0])
+    cluster_policies = [BayesianPolicy(metadata, alpha=0.5).fit(obs, acs) for obs, acs in zip(cluster_obs, cluster_acs)]
+    universal_obs, universal_acs = aggregate_policies(observations, actions, [0] * len(policies))
+    universal_policy = BayesianPolicy(metadata, alpha=0.5).fit(universal_obs[0], universal_acs[0])
 
-    # grid visualization
-    if env_id in ('deep-sea-treasure-v0', 'minecart-v0'):
-        c_obs, c_ac = aggregate_policies(obs_seq, ac_seq, labels[0])
-        for c in range(k):
-            policy = BayesianPolicy(env_id, alpha=0.5).fit(c_obs[c], c_ac[c])
-            title = f'Behavior Cluster {c + 1}: {len(c_obs[c])} episodes'
-            color = colors[0][c]
-            figs.extend([
-                #(grid_trajectories(c_obs[c], title=title, color=color, alpha=0.01), f"trajectory{c}.png"),
-                #(grid_arrows(policy, title=title, color=color), f"arrows{c}.png"),
-                (temporal_alignment(c_ac[c], env_id, title=title), f'temporal_alignment{c}.png'),
-                (decision_tree(*tree_features(c_obs[c], c_ac[c]), env_id=env_id, title=title), f'decision_tree{c}.png')
-            ])
-        obs_univ, acs_univ = aggregate_policies(obs_seq, ac_seq, [0] * len(policies))
-        policy_univ = BayesianPolicy(env_id, alpha=0.5).fit(obs_univ[0], acs_univ[0])
+    # Plotting
+    # todo add boxplot reward
+    figs = [(cluster_scatter(behavior_features, labels[0], colors=colors['warm']), f"behavior_scatter.png"),
+            (sankey(*cluster_connections(labels), colors), "sankey.png")]
 
-        title = f'Universal Policy: {len(obs_univ[0])} episodes'
+    for c_id in range(k):
+        title = f'Behavior Cluster {c_id + 1}: {len(cluster_obs[c_id])} episodes'
+        color = colors['warm'][c_id]
         figs.extend([
-            #(grid_trajectories(obs_univ[0], title=title, color='white'), "trajectory_universal.png"),
-            #(grid_arrows(policy_univ, title=title), "arrows_universal.png"),
-            (temporal_alignment(acs_univ[0], env_id, title=title), "temporal_alignment_universal.png"),
-            (decision_tree(*tree_features(obs_univ[0], acs_univ[0]), env_id=env_id, title=title), "decision_tree_universal.png")
+            #(grid_trajectories(c_obs[c], title=title, color=color, alpha=0.01), f"trajectory{c}.png"),
+            (minecart_trajectories(cluster_obs[c_id], cluster_acs[c_id], title=title, color=color), f"trajectory{c_id}.png"),
+            (temporal_alignment(cluster_acs[c_id], metadata['actions'], time_range=(0, max_len), title=title), f'temporal_alignment{c_id}.png'),
+            (decision_tree(*tree_features(cluster_obs[c_id], cluster_acs[c_id]), metadata, title=title), f'decision_tree{c_id}.png')
         ])
+
+    title = f'Universal Policy: {len(universal_obs[0])} episodes'
+    figs.extend([
+        #(grid_trajectories(obs_univ[0], title=title, color='white'), "trajectory_universal.png"),
+        (temporal_alignment(universal_acs[0], metadata['actions'], title=title), "temporal_alignment_universal.png"),
+        (decision_tree(*tree_features(universal_obs[0], universal_acs[0]), metadata, title=title), "decision_tree_universal.png")
+    ])
+
+
+    # Saving
 
     if save:
         for fig, filename in figs:
